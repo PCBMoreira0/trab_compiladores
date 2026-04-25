46dc95ec6678001e9b3c38e64f2dd9f9496a92e4
gdac <gabrieldalcs@gmail.com>
2026-04-25 16:34:35 -0300

    add argv to be more flexible with racket files

--- main_compilador.c
+++ main_compilador.c
@@ -3,9 +3,12 @@
 #include "libs/afd.h"
 #include "libs/scanner.h"
 
-int main(int argc, char* argv) {
+int main(int argc, char **argv) {
+     if (argc < 2){
+        printf("O primeiro argumento deve ser o arquivo racket que deseja testar!\n");
+        return 1;
+    }
     printf("[*] Iniciando o Compilador Racket...\n");
-
     printf("[*] Carregando a maquina de estados (afd_serializado.bin)...\n");
     FILE *f = fopen("afd_serializado.bin", "rb");
     if (!f) {
@@ -16,9 +19,8 @@ int main(int argc, char* argv) {
     Automato_AFD *dfa = AFDDeserialize(f);
     fclose(f);
     printf("[+] Scanner carregado com sucesso! (%d estados)\n", dfa->num_states);
-
-    printf("[*] Analisando arquivo 'programa.rkt'...\n");
-    TokenList *fita = tokenize_file(dfa, "programa.rkt");
+    printf("[*] Analisando arquivo %s...\n", argv[1]);
+    TokenList *fita = tokenize_file(dfa, argv[1]);
 
     if (fita) {
         print_token_list(fita);
@@ -32,7 +34,6 @@ int main(int argc, char* argv) {
 
         free_token_list(fita);
     }
-
     afdFree(dfa);
     return 0;
 }
